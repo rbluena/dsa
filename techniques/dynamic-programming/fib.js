659674290a090f65ce2c8a82caced75f05676317
Rabii Luena <itisrabii@gmail.com>
2022-09-09 10:50:07 +0300

    (experiment): dynamic programming with fibonacci

---
+++ techniques/dynamic-programming/fib.js
@@ -0,0 +1,13 @@
+// const memo = {};
+
+function fib(n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n <= 1) return 1;
+
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+
+  return memo[n];
+}
+
+function fibonacciNth() {}
+``;
